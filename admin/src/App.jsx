539488403ea09b738a06fe9a8ec03357676b3f25
lequythien <zacubj2@gmail.com>
2025-05-07 09:36:33 +0700

    update code 07/05/2025 09h36

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,13 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-import Employees from "./pages/Employees";
 import Departments from "./pages/Departments";
 import Projects from "./pages/Projects";
 import Jobs from "./pages/Jobs";
-import Positions from "./pages/Positions";
-import Attendance from "./pages/Attendance";
-import Payroll from "./pages/Payroll";
+import Member from "./pages/Member";
+import ProjectProgress from "./pages/ProjectProgress";
+import MemberDetail from "./pages/MemberDetail";
 
 const App = () => {
   return (
@@ -20,13 +19,13 @@ const App = () => {
         <main className="flex-1 p-4 bg-gray-100 overflow-y-auto">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/employees" element={<Employees />} />
+            <Route path="/member" element={<Member />} />
+            <Route path="/memberdetail" element={<MemberDetail />} />
+
             <Route path="/departments" element={<Departments />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/jobs" element={<Jobs />} />
-            <Route path="/positions" element={<Positions />} />
-            <Route path="/attendance" element={<Attendance />} />
-            <Route path="/payroll" element={<Payroll />} />
+            <Route path="/projectprogress" element={<ProjectProgress />} />
           </Routes>
         </main>
       </div>
