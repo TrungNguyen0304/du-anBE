fec4aa4619d2454a5b86712dc62e0aedbf1c4732
TrungNguyen0304 <TrungNguyen15989@gmail.com>
2025-05-15 11:35:14 +0700

    thong bao

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { login, saveFcmToken } = require("../controller/user.js");
+const authenticateJWT = require("../middleware/auth.js");
 require("../config/passport.js");
 
 const router = express.Router();
@@ -7,6 +8,6 @@ const router = express.Router();
 // Route đăng nhập
 router.post("/login", login);
 
-router.post("/fcm-token", saveFcmToken)
+router.post('/fcm-token', authenticateJWT, saveFcmToken);
 
 module.exports = router;
