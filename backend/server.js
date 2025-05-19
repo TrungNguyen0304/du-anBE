2317e226a485adf2f7cd45062f39cb09eda07f5d
lequythien <zacubj2@gmail.com>
2025-05-19 15:47:41 +0700

    Sửa lỗi vercel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const http = require("http");
 const { setupSocket } = require("./socket/socketHandler.js");
-const {startScheduleCheck} = require("./socket/socketSchedule.js")
+const { startScheduleCheck } = require("./socket/socketSchedule.js");
 
 const authRoute = require("./route/authRoute.js");
 const userRoute = require("./route/userRoute.js");
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app); // Tạo server để dùng với socket
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // socket cũng cần config đúng origin
+    origin: ["http://localhost:5173", "https://quanlynhansu-seven.vercel.app"], // socket cũng cần config đúng origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
